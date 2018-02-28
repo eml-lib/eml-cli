8a280db3b3ee3e05f37e9cd1f128c54d38e9a5ae
vtrushin <monospectr@gmail.com>
2018-02-28 03:10:23 +0300

    Added first script

--- bin/index.js
+++ bin/index.js
@@ -1,16 +1,25 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
-var program = require('commander');
-var eml = require('eml');
+const fs = require('fs');
+const path = require('path');
+const program = require('commander');
+const glob = require('glob');
+const eml = require('eml');
 
 program
     .version('0.0.1-alpha', '-v, --version')
     .option('-i, --input <file ...>', 'Input EML file')
-    .option('-o, --output <file ...>', 'Output HTML file')
+    .option('-o, --output [file ...]', 'Output HTML file')
     .parse(process.argv);
 
-const emlFile = fs.readFileSync(program.input, 'utf8');
-const html = eml.parse(emlFile);
+glob(program.input, null, function(err, paths) {
+    paths.forEach(function(filePath) {
+        const extension = path.extname(filePath);
+        const dirname = path.dirname(filePath);
+        const basename = path.basename(filePath, extension);
+        const emlFile = fs.readFileSync(filePath, 'utf8');
+        const html = eml(emlFile);
 
-fs.writeFileSync(program.output, html, 'utf8');
+        fs.writeFileSync(dirname + '/' + basename + '.html', html, 'utf8');
+    });
+});
