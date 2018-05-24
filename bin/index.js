8684f6c65ea3a2d7caf21b0b4c57fdf679519edf
Vladislav Trushin <vtrushin@avito.ru>
2018-05-24 18:14:58 +0300

    Moved code outside cli file

--- bin/index.js
+++ bin/index.js
@@ -1,25 +1,3 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
-const program = require('commander');
-const glob = require('glob');
-const eml = require('eml');
-
-program
-    .version('0.0.1-alpha', '-v, --version')
-    .option('-i, --input <file ...>', 'Input EML file')
-    .option('-o, --output [file ...]', 'Output HTML file')
-    .parse(process.argv);
-
-glob(program.input, null, function(err, paths) {
-    paths.forEach(function(filePath) {
-        const extension = path.extname(filePath);
-        const dirname = path.dirname(filePath);
-        const basename = path.basename(filePath, extension);
-        const emlFile = fs.readFileSync(filePath, 'utf8');
-        const html = eml(emlFile);
-
-        fs.writeFileSync(dirname + '/' + basename + '.html', html, 'utf8');
-    });
-});
+require('..')();
